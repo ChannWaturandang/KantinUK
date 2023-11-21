f145647257a5bd1b5b008ebd84591e5ef84c0a60
xVyeRoMyu <117585885+xVyeRoMyu@users.noreply.github.com>
2023-11-21 11:12:08 +0800

    UPDATE ABOUT

---
+++ AboutControl.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Makanan.Controls
+{
+    partial class AboutControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutControl));
+            this.adminLogo = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox8 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox7 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // adminLogo
+            // 
+            this.adminLogo.AutoSize = true;
+            this.adminLogo.Font = new System.Drawing.Font("Harlow Solid Italic", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.adminLogo.ForeColor = System.Drawing.Color.Teal;
+            this.adminLogo.Location = new System.Drawing.Point(39, 38);
+            this.adminLogo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.adminLogo.Name = "adminLogo";
+            this.adminLogo.Size = new System.Drawing.Size(184, 71);
+            this.adminLogo.TabIndex = 32;
+            this.adminLogo.Text = "About";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoScrollMargin = new System.Drawing.Size(0, 350);
+            this.panel1.Controls.Add(this.richTextBox8);
+            this.panel1.Controls.Add(this.richTextBox7);
+            this.panel1.Controls.Add(this.richTextBox6);
+            this.panel1.Controls.Add(this.richTextBox5);
+            this.panel1.Controls.Add(this.richTextBox4);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.richTextBox3);
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(973, 1048);
+            this.panel1.TabIndex = 45;
+            // 
+            // richTextBox8
+            // 
+            this.richTextBox8.BackColor = System.Drawing.Color.Black;
+            this.richTextBox8.Font = new System.Drawing.Font("Calibri", 13F);
+            this.richTextBox8.ForeColor = System.Drawing.Color.White;
+            this.richTextBox8.Location = new System.Drawing.Point(34, 1177);
+            this.richTextBox8.Name = "richTextBox8";
+            this.richTextBox8.ReadOnly = true;
+            this.richTextBox8.Size = new System.Drawing.Size(335, 52);
+            this.richTextBox8.TabIndex = 41;
+            this.richTextBox8.Text = "Deadline: 21 November 2023";
+            // 
+            // richTextBox7
+            // 
+            this.richTextBox7.BackColor = System.Drawing.Color.Orange;
+            this.richTextBox7.Font = new System.Drawing.Font("Calibri", 13F);
+            this.richTextBox7.ForeColor = System.Drawing.Color.Black;
+            this.richTextBox7.Location = new System.Drawing.Point(311, 985);
+            this.richTextBox7.Name = "richTextBox7";
+            this.richTextBox7.ReadOnly = true;
+            this.richTextBox7.Size = new System.Drawing.Size(623, 186);
+            this.richTextBox7.TabIndex = 40;
+            this.richTextBox7.Text = "Extra:\nKelompok 9\nVisual Programming Class\nFinal Project\n2 Weeks of development i" +
+    "n estimated 22 hours of work.";
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.BackColor = System.Drawing.Color.Indigo;
+            this.richTextBox6.Font = new System.Drawing.Font("Calibri", 13F);
+            this.richTextBox6.ForeColor = System.Drawing.Color.White;
+            this.richTextBox6.Location = new System.Drawing.Point(34, 858);
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.ReadOnly = true;
+            this.richTextBox6.Size = new System.Drawing.Size(474, 121);
+            this.richTextBox6.TabIndex = 39;
+            this.richTextBox6.Text = "Special Thanks to:\n~ Hendy (Helper) ✨\n~ Sir Rolly (Teacher)";
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.BackColor = System.Drawing.Color.MidnightBlue;
+            this.richTextBox5.Font = new System.Drawing.Font("Calibri", 13F);
+            this.richTextBox5.ForeColor = System.Drawing.Color.White;
+            this.richTextBox5.Location = new System.Drawing.Point(470, 651);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.ReadOnly = true;
+            this.richTextBox5.Size = new System.Drawing.Size(464, 201);
+            this.richTextBox5.TabIndex = 38;
+            this.richTextBox5.Text = "Developers:\n~ Chan (UX, Supervisor & Backend ) 😎📝\n~ Jordy (Programmer) 💻\n~ Ale" +
+    "x (UI & Programmer) 🔧\n~ Sky (UI Designer) 🎨";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.BackColor = System.Drawing.Color.Gold;
+            this.richTextBox4.Font = new System.Drawing.Font("Calibri", 13F);
+            this.richTextBox4.ForeColor = System.Drawing.Color.Black;
+            this.richTextBox4.Location = new System.Drawing.Point(375, 42);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.ReadOnly = true;
+            this.richTextBox4.Size = new System.Drawing.Size(559, 128);
+            this.richTextBox4.TabIndex = 37;
+            this.richTextBox4.Text = "Introduction:\nIni adalah sebuah aplikasi desktop yang dibuat untuk system kasir d" +
+    "i canteen Unklab.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Harlow Solid Italic", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Teal;
+            this.label1.Location = new System.Drawing.Point(22, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 71);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "About";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BackColor = System.Drawing.Color.Brown;
+            this.richTextBox3.Font = new System.Drawing.Font("Calibri", 13F);
+            this.richTextBox3.ForeColor = System.Drawing.Color.White;
+            this.richTextBox3.Location = new System.Drawing.Point(34, 344);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(900, 301);
+            this.richTextBox3.TabIndex = 35;
+            this.richTextBox3.Text = resources.GetString("richTextBox3.Text");
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.LimeGreen;
+            this.richTextBox1.Font = new System.Drawing.Font("Calibri", 13F);
+            this.richTextBox1.ForeColor = System.Drawing.Color.Black;
+            this.richTextBox1.Location = new System.Drawing.Point(34, 176);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(514, 162);
+            this.richTextBox1.TabIndex = 33;
+            this.richTextBox1.Text = "Objectives:\nTujuan kami untuk membuat aplikasi ini, tentu saja untuk memudahkan k" +
+    "erjanya kasir dalam pengambilan data dan pemesanan pelanggan.";
+            // 
+            // AboutControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.adminLogo);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "AboutControl";
+            this.Size = new System.Drawing.Size(973, 1048);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label adminLogo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox richTextBox8;
+        private System.Windows.Forms.RichTextBox richTextBox7;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
